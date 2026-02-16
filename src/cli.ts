63451387d490b58c1eb57c06fe3ceacd7009c954
Jaeymo <jaey.cyber@gmail.com>
2026-02-16 16:12:56 -0600

    feat: create CLI tool

---
+++ src/cli.ts
@@ -0,0 +1,20 @@
+import { Command } from "commander";
+import { generateMapping } from "./generator.js";
+
+const program = new Command();
+
+program
+    .name("toybox")
+    .description("Generate rojo mappings from a Roblox directory")
+    .version("1.0.0");
+
+program
+    .command("generate")
+    .requiredOption("-i, --input <path>", "Input roblox directory")
+    .option("-f, --folder <name>", "Folder inside Roblox to map", "Asset")
+    .requiredOption("-o, --output <path>", "Path to .project.json")
+    .action((options) => {
+        generateMapping(options.input, options.output, options.folder);
+    });
+
+program.parse(process.argv);
